0639c4e380c8318c0c3e66d4af0aae6035002f02
ChandlerFord <ChandlerFord452@gmail.com>
2016-03-05 23:58:11 -0500

    Test 3
    Runs with no error, had wrong connection info... No email showing up in
    inbox though

--- Client.py
+++ Client.py
@@ -2,15 +2,15 @@ __author__ = 'Chandler'
 
 import smtplib
 
-sender = "test@gmail.com"
+sender = "Test@Duracrypt.com"
 reciever = "chandlerford452@gmail.com"
 message = "Test Message"
 
 
 server = smtplib.SMTP()
-server.connect('smtpauth.registerapi.com', 26)
+server.connect('smtp.dotster.com', 587)
 server.login('Test@Duracrypt.com', 'Th!$Is4T3stP4$$werd')
 server.set_debuglevel(1)
 server.sendmail(sender, reciever, message)
-print "Successfully sent email"
 server.quit()
+print "Successfully sent email"
