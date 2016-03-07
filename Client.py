a2af113d150c6abb30a9ec53d603a63819a296b5
ChandlerFord <ChandlerFord452@gmail.com>
2016-03-07 01:50:10 -0500

    Update
    +Login info simplified
    +Added Subject Line
    +Formatting

--- Client.py
+++ Client.py
@@ -1,16 +1,24 @@
 __author__ = 'Chandler'
 
 import smtplib
+import email.utils
+from email.mime.text import MIMEText
 
-sender = "Test@Duracrypt.com"
-reciever = "Test@Duracrypt.com"
-message = "Test Message"
+#Login Info:
+Username = 'Test@Duracrypt.com'
+Password = 'Th!$Is4T3stP4$$werd'
 
+#Email Info
+message = MIMEText("This is a test message")
+message['To'] = email.utils.formataddr(('Reciever', 'Test@Duracrypt.com'))
+message['From'] = email.utils.formataddr(('Sender', 'Test@Duracrypt.com'))
+message['Subject'] = 'TEST'
 
+#Send
 server = smtplib.SMTP()
 server.connect('smtp.dotster.com', 587)
-server.login('Test@Duracrypt.com', 'Th!$Is4T3stP4$$werd')
+server.login(Username , Password)
 server.set_debuglevel(1)
-server.sendmail(sender, reciever, message)
+server.sendmail('Test@Duracrypt.com', 'Test@Duracrypt.com', message.as_string())
 server.quit()
 print "Successfully sent email"
